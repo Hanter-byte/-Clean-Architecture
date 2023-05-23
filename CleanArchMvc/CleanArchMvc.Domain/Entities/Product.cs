e8bad5089458bd99ca247cd7d0e8c3181b7efb60
João Pedro <jp663687@gmail.com>
2023-05-23 14:12:19 -0300

    Context

--- CleanArchMvc/CleanArchMvc.Domain/Entities/Product.cs
+++ CleanArchMvc/CleanArchMvc.Domain/Entities/Product.cs
@@ -23,7 +23,7 @@ namespace CleanArchMvc.Domain.Entities
 
         public Product(int id, string name, string description, decimal price, int stock, string image)
         {
-            DomainExceptionValidation.When(id < 0, "invalid Id value");
+            DomainExceptionValidation.When(id < 0, "Invalid Id value");
             Id = id;
             ValidateDomain(name, description, price, stock, image);
         }
@@ -48,7 +48,7 @@ namespace CleanArchMvc.Domain.Entities
 
             DomainExceptionValidation.When(stock < 0, "Invalid stock value");
 
-            DomainExceptionValidation.When(image.Length > 250, "Invalid image name, maximum 250 characters");
+            DomainExceptionValidation.When(image?.Length > 250, "Invalid image name, maximum 250 characters");
 
             Name = name;
             Description = description;
